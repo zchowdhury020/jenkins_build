7e20547bf7daca7ef7eeeb808d92b0db6e5d375d
zchowdhury020 <132600997+zchowdhury020@users.noreply.github.com>
2023-06-08 12:30:43 -0400

    Update SampleJavaProject.java

--- src/sample/java/project/SampleJavaProject.java
+++ src/sample/java/project/SampleJavaProject.java
@@ -19,7 +19,7 @@ public class SampleJavaProject implements Runnable {
 
     /** The delay between printed messages. */
     private static final long PRINT_DELAY = 1000L;
-  this 
+    
     /** The name to be printed in the output message. */
     @Getter @Setter @NonNull
     @Parameter(names = "--name", description = "set the user's name",
